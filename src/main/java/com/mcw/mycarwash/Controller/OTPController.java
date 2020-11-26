5014b602d4b5cd7a1ada9cd3b87d6e86d8e9466b
Chanaka <rwp.chanaka@gmail.com>
2020-11-27 00:12:40 +0530

    otp

--- src/main/java/com/mcw/mycarwash/Controller/OTPController.java
+++ src/main/java/com/mcw/mycarwash/Controller/OTPController.java
@@ -1,6 +1,11 @@
 package com.mcw.mycarwash.Controller;
 
+import com.mcw.mycarwash.Model.OTPModel;
 import com.mcw.mycarwash.ServiceImpl.OtpService;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.slf4j.Logger;
@@ -15,62 +20,47 @@ import java.util.Map;
 @Controller
 public class OTPController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Map<String, OTPModel> otp_data = new HashMap<>();
 
     @Autowired
     public OtpService otpService;
 
-    @GetMapping("/generateOtp")
-    public String generateOtp() {
-
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String username = auth.getName();
-
-        int otp = otpService.generateOTP(username);
-
-        logger.info("OTP : " + otp);
-
-//        //Generate The Template to send OTP
-//        EmailTemplate template = new EmailTemplate("SendOtp.html");
-//
-//        Map<String,String> replacements = new HashMap<String,String>();
-//        replacements.put("user", username);
-//        replacements.put("otpnum", String.valueOf(otp));
-//
-//        String message = template.getTemplate(replacements);
-//
-//        myEmailService.sendOtpMessage("shrisowdhaman@gmail.com", "OTP -SpringBoot", message);
-
-        return otp + " otppage";
+    @PostMapping("/generateOtp/{mobilenumber}")
+    public ResponseEntity<Object> sendOTP(@PathVariable String mobilenumber) {
+        OTPModel otpModel = new OTPModel();
+        otpModel.setMobileNumber(mobilenumber);
+        otpModel.setOtp(String.valueOf(((int) (Math.random() * (1000 - 1000))) + 1000));
+        otpModel.setExpiryTime(System.currentTimeMillis() / 20000);
+        otp_data.put(mobilenumber, otpModel);
+        // OTP Sending function here, Email and SMS
+        return new ResponseEntity<>("OTP Send", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/validateOtp", method = RequestMethod.GET)
-    public @ResponseBody String validateOtp(@RequestParam("otpnum") int otpnum) {
 
-        final String SUCCESS = "Entered Otp is valid";
-
-        final String FAIL = "Entered Otp is NOT valid. Please Retry!";
-
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String username = auth.getName();
-
-        logger.info(" Otp Number : " + otpnum);
-
-        //Validate the Otp
-        if (otpnum >= 0) {
-            int serverOtp = otpService.getOtp(username);
-
-            if (serverOtp > 0) {
-                if (otpnum == serverOtp) {
-                    otpService.clearOTP(username);
-                    return ("Entered Otp is valid");
+    @PostMapping(value = "/validateOtp/{mobilenumber}")
+    public ResponseEntity<Object> validateOtp(@PathVariable String mobilenumber, @RequestBody OTPModel otpModel) {
+        if (otpModel.getOtp() == null || otpModel.getOtp().length() <= 0) {
+            return new ResponseEntity<>("Please Enter Correct OTP", HttpStatus.OK);
+        }
+        if (otp_data.containsKey(mobilenumber)) {
+            OTPModel otpModel1 = otp_data.get(mobilenumber);
+            if (otpModel1 != null) {
+                if (otpModel1.getExpiryTime() <= System.currentTimeMillis()) {
+                    if (otpModel.getOtp().equals(otpModel1.getOtp())) {
+                        otp_data.remove(mobilenumber);
+                        return new ResponseEntity<>("OTP Verified", HttpStatus.OK);
+                    } else {
+                        return new ResponseEntity<>("Invalid OTP", HttpStatus.BAD_REQUEST);
+                    }
                 } else {
-                    return SUCCESS;
+                    otp_data.remove(mobilenumber);
+                    return new ResponseEntity<>("OTP Expired", HttpStatus.BAD_REQUEST);
                 }
             } else {
-                return FAIL;
+                otp_data.remove(mobilenumber);
+                return new ResponseEntity<>("Something went wrong", HttpStatus.BAD_REQUEST);
             }
-        } else {
-            return FAIL;
         }
+        return new ResponseEntity<>("Mobile Number not found", HttpStatus.NOT_FOUND);
     }
 }
